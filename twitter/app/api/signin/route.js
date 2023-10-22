1dbe01adcb022d4ed5bd9414567809d7f1962ac8
Aman0246 <amankashyap0246@gmail.com>
2023-10-22 22:58:04 +0530

    login/Register

---
+++ twitter/app/api/signin/route.js
@@ -0,0 +1,29 @@
+import { prisma } from "@/prisma";
+import { NextResponse } from "next/server";
+
+//Login
+export async function POST(request) {
+  try {
+    const {  email, password } = await request.json();
+    if ( !email || !password)
+      return NextResponse.json({ status: false, message: "empty Filds" });
+
+    const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/;
+    if (!emailPattern.test(email)) {
+      return NextResponse.json({ status: false, message: "Invalid email" });
+    }
+
+    let userExist = await prisma.user.findFirst({ where: { email } });
+    if (!userExist)
+      return NextResponse.json({ status: false, message: "Register first" },{status:400});
+
+    if(userExist.password!=password)return NextResponse.json({ status: false, message: "Worng Password" },{status:400});
+    
+    return NextResponse.json(
+      { status: true, message: "Login", userExist },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
